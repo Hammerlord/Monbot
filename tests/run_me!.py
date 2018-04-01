cf686eba63e29769fa3d330e74d443503596cd93
Hammerlord <nepharus@gmail.com>
2018-04-01 14:22:40 -0700

    Add a test runner file.

---
+++ tests/run_me!.py
@@ -0,0 +1,28 @@
+import unittest
+from typing import Type
+
+from tests.character.character import CharacterTests
+from tests.character.npc import NPCTests
+from tests.character.player import PlayerTests
+from tests.elemental.combat_elemental import CombatElementalTests
+from tests.elemental.elemental import ElementalTests
+from tests.team.combat_team import CombatTeamTests
+from tests.team.team import TeamTests
+
+suites = [CharacterTests,
+          NPCTests,
+          PlayerTests,
+          ElementalTests,
+          CombatElementalTests,
+          TeamTests,
+          CombatTeamTests]
+
+
+def load_test(tests: Type[unittest.TestCase]):
+    return unittest.TestLoader().loadTestsFromTestCase(tests)
+
+
+all_tests = unittest.TestSuite([load_test(suite) for suite in suites])
+
+if __name__ == "__main__":
+    unittest.main()
