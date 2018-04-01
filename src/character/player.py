56f692fa62e6b8d9cd7dd43cdbf6d9b803b5e76d
Hammerlord <nepharus@gmail.com>
2018-04-01 15:43:51 -0700

    Add Player class.

---
+++ src/character/player.py
@@ -0,0 +1,16 @@
+from src.character.character import Character
+
+
+class Player(Character):
+    def __init__(self, user):
+        super().__init__()
+        self._is_busy = False
+        self._current_interface = None  # TODO
+        self.id = user.id
+        self.set_nickname(user.name)
+
+    @staticmethod
+    def create_player(user, starter: Elemental) -> 'Player':
+        new_profile = Player(user)
+        new_profile.add_elemental(starter)
+        return new_profile
