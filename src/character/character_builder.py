b812e65f1832afe8e1c617252aeec20f4ecb8ce1
Hammerlord <nepharus@gmail.com>
2018-04-01 21:58:04 -0700

    Add PlayerBuilder and fix the customization of other Builder outputs.

--- src/character/character_builder.py
+++ src/character/character_builder.py
@@ -3,35 +3,54 @@ from typing import Type
 from src.character.character import Character
 from src.character.npc import NPC
 from src.character.player import Player
+from tests.test_user import UserBuilder
 
 
 class CharacterBuilder:
     def __init__(self):
-        self.character = Character()
+        self._level = 1
 
     def build(self) -> Character:
-        return self.character
+        character = Character()
+        while character.level < self._level:
+            exp = character.exp_to_level
+            character.add_exp(exp)
+        return character
 
     def with_level(self, level: int) -> 'CharacterBuilder':
-        while self.character.level < level:
-            exp = self.character.exp_to_level
-            self.character.add_exp(exp)
+        self._level = level
         return self
 
 
 class NPCBuilder:
     def __init__(self):
-        self.npc = NPC()
+        self._level = 1
+        self.opponent = PlayerBuilder().build()
 
     def build(self) -> NPC:
-        return self.npc
+        npc = NPC()
+        while npc.level < self._level:
+            exp = npc.exp_to_level
+            npc.add_exp(exp)
+        return npc
 
     def with_level(self, level: int) -> 'NPCBuilder':
-        while self.npc.level < level:
-            exp = self.npc.exp_to_level
-            self.npc.add_exp(exp)
+        self._level = level
         return self
 
     def with_opponent(self, opponent: Type[Character]) -> 'NPCBuilder':
-        self.npc.generate_team(opponent)
+        self.opponent = opponent
         return self
+
+
+class PlayerBuilder:
+    def __init__(self):
+        self._level = 1
+        self.user = UserBuilder().build()
+
+    def build(self) -> Player:
+        player = Player(self.user)
+        while player.level < self._level:
+            exp = player.exp_to_level
+            player.add_exp(exp)
+        return player
