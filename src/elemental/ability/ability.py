0be05dd394ed38d676378855589495fc1f51a470
Hammerlord <nepharus@gmail.com>
2018-04-22 18:58:09 -0700

    Remove @properties from LearnableAbility.

--- src/elemental/ability/ability.py
+++ src/elemental/ability/ability.py
@@ -60,92 +60,32 @@ class LearnableAbility:
 
     def __init__(self,
                  ability: Ability):
-        self._ability = ability
-        self._level_required = 0
-        self._p_att_rank_required = 0  # Attribute ranks required, eg. Ferocity, Attunement, etc.
-        self._m_att_rank_required = 0
-        self._p_def_rank_required = 0
-        self._hp_rank_required = 0
-        self._m_def_rank_required = 0
-        self._speed_rank_required = 0
+        self.ability = ability
+        self.level_required = 0
+        self.p_att_rank_required = 0  # Attribute ranks required, eg. Ferocity, Attunement, etc.
+        self.m_att_rank_required = 0
+        self.p_def_rank_required = 0
+        self.hp_rank_required = 0
+        self.m_def_rank_required = 0
+        self.speed_rank_required = 0
 
     @property
     def ability_id(self) -> int:
-        return self._ability.id
+        return self.ability.id
 
     @property
     def name(self) -> str:
-        return self._ability.name
-
-    @property
-    def ability(self) -> Ability:
-        return self._ability
-
-    @property
-    def level_required(self) -> int:
-        return self._level_required
-
-    @level_required.setter
-    def level_required(self, level: int) -> None:
-        self._level_required = level
-
-    @property
-    def p_att_rank_required(self) -> int:
-        return self._p_att_rank_required
-
-    @p_att_rank_required.setter
-    def p_att_rank_required(self, amount: int) -> None:
-        self._p_att_rank_required = amount
-
-    @property
-    def m_att_rank_required(self) -> int:
-        return self._m_att_rank_required
-
-    @m_att_rank_required.setter
-    def m_att_rank_required(self, amount: int) -> None:
-        self._m_att_rank_required = amount
-
-    @property
-    def p_def_rank_required(self) -> int:
-        return self._p_def_rank_required
-
-    @p_def_rank_required.setter
-    def p_def_rank_required(self, amount: int) -> None:
-        self._p_def_rank_required = amount
-
-    @property
-    def hp_rank_required(self) -> int:
-        return self._hp_rank_required
-
-    @hp_rank_required.setter
-    def hp_rank_required(self, amount: int) -> None:
-        self._hp_rank_required = amount
-
-    @property
-    def m_def_rank_required(self) -> int:
-        return self._m_def_rank_required
-
-    @m_def_rank_required.setter
-    def m_def_rank_required(self, amount: int) -> None:
-        self._m_def_rank_required = amount
-
-    @property
-    def speed_rank_required(self) -> int:
-        return self._speed_rank_required
-
-    @speed_rank_required.setter
-    def speed_rank_required(self, amount: int) -> None:
-        self._speed_rank_required = amount
+        return self.ability.name
 
     def are_requirements_fulfilled(self, elemental) -> bool:
         """
         :param elemental: The Elemental trying to learn this ability.
         """
         return \
-            elemental.level >= self._level_required and \
-            elemental.ferocity >= self._p_att_rank_required and \
-            elemental.attunement >= self._m_att_rank_required and \
-            elemental.sturdiness >= self._p_def_rank_required and \
-            elemental.resolve >= self._hp_rank_required and \
-            elemental.resistance >= self._m_def_rank_required and \
-            elemental.swiftness >= self._speed_rank_required
+            elemental.level >= self.level_required and \
+            elemental.ferocity >= self.p_att_rank_required and \
+            elemental.attunement >= self.m_att_rank_required and \
+            elemental.sturdiness >= self.p_def_rank_required and \
+            elemental.resolve >= self.hp_rank_required and \
+            elemental.resistance >= self.m_def_rank_required and \
+            elemental.swiftness >= self.speed_rank_required
