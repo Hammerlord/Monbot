32fdcad25953b6fee58696a554707df498db2fa0
Hammerlord <nepharus@gmail.com>
2018-10-06 19:54:35 -0700

    Add more shard items for the remaining elements.

--- src/items/materials.py
+++ src/items/materials.py
@@ -1,14 +1,16 @@
 
-from src.core.constants import MANA_SHARD, EARTH_SHARD, LIGHTNING, FIRE, WATER, WIND
+from src.core.constants import MANA_SHARD, EARTH_SHARD, LIGHTNING, FIRE, WATER, WIND, DARK, LIGHT, CHAOS
+from src.core.elements import Elements
 from src.items.item import Item, ItemTypes, ItemSubcategories
 
 
 class Shard(Item):
     def __init__(self):
         super().__init__()
-        self.subcategory = ItemSubcategories.NONE
+        self.subcategory = ItemSubcategories.SHARD
         self.item_type = ItemTypes.MATERIAL
         self.sell_price = None
+        self.element = Elements.NONE
 
 
 class ManaShard(Shard):
@@ -25,6 +27,7 @@ class EarthShard(Shard):
         self.name = 'Earth Shard'
         self.description = "A crystal infused with earthen mana. Weighty."
         self.icon = EARTH_SHARD
+        self.element = Elements.EARTH
 
 
 class LightningShard(Shard):
@@ -33,6 +36,7 @@ class LightningShard(Shard):
         self.name = 'Lightning Shard'
         self.description = "A crystal sparking with lightning mana. It hums softly with an electrical current."
         self.icon = LIGHTNING
+        self.element = Elements.LIGHTNING
 
 
 class FireShard(Shard):
@@ -41,6 +45,7 @@ class FireShard(Shard):
         self.name = 'Fire Shard'
         self.description = "A crystal glowing orange at its core. Warm to the touch."
         self.icon = FIRE
+        self.element = Elements.FIRE
 
 
 class WaterShard(Shard):
@@ -49,6 +54,7 @@ class WaterShard(Shard):
         self.name = 'Water Shard'
         self.description = "A radiant blue crystal."
         self.icon = WATER
+        self.element = Elements.WATER
 
 
 class WindShard(Shard):
@@ -57,3 +63,31 @@ class WindShard(Shard):
         self.name = 'Wind Shard'
         self.description = "A colourless crystal. A light breeze seems to pick up around it."
         self.icon = WIND
+        self.element = Elements.WIND
+
+
+class ShadowShard(Shard):
+    def __init__(self):
+        super().__init__()
+        self.name = 'Shadow Shard'
+        self.description = "A shard of pure darkness. No light can pass through it."
+        self.icon = DARK
+        self.element = Elements.DARK
+
+
+class LightShard(Shard):
+    def __init__(self):
+        super().__init__()
+        self.name = 'Light Shard'
+        self.description = "A shard of pure light. It glows like the sun."
+        self.icon = LIGHT
+        self.element = Elements.LIGHT
+
+
+class ChaosShard(Shard):
+    def __init__(self):
+        super().__init__()
+        self.name = 'Chaos Shard'
+        self.description = "A crystal emitting an eerie pallid glow."
+        self.icon = CHAOS
+        self.element = Elements.CHAOS
